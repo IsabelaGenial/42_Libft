060a613aefed0ab88c7e05022f2691babd803bbd
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-19 17:09:55 -0300

    refactor(ft_memmove): Norm correction

--- src/ft_memmove.c
+++ src/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: igenial <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 13:40:55 by igenial           #+#    #+#             */
-/*   Updated: 2023/05/19 13:47:57 by igenial          ###   ########.fr       */
+/*   Updated: 2023/05/19 15:51:55 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	if (dest <= src)
-		return (ft_memcpy(dest, src, n);
-	while(n-- > 0)
-		((char *)dest)[n] = ((char *)src);
+		return (ft_memcpy(dest, src, n));
+	while (n-- > 0)
+		((char *)dest)[n] = ((char *)src)[n];
 	return (dest);
 }
