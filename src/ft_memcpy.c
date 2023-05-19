2e8673e742cf0c71c702311980d729b7efda7fc4
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-19 17:10:51 -0300

    refactor(ft_memcpy): Norm correction

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -6,15 +6,15 @@
 /*   By: igenial <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 13:35:09 by igenial           #+#    #+#             */
-/*   Updated: 2023/05/19 13:39:42 by igenial          ###   ########.fr       */
+/*   Updated: 2023/05/19 15:45:04 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	while (n-- > 0)
-		((unsigned char*)dest)[n] = ((unsigned char *)src)[n]
+		((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
 	return (dest);
 }
