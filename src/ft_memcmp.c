5da216c57ca25120df22baede42178acb2e0e71a
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-20 18:00:42 -0300

    fix(ft_memcmp): logic error correction.

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: igenial <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 16:04:16 by igenial           #+#    #+#             */
-/*   Updated: 2023/05/19 16:10:02 by igenial          ###   ########.fr       */
+/*   Updated: 2023/05/20 15:38:53 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
+	unsigned char *text[2];
 
+	text[0] = ((unsigned char *)s1);
+	text[1] = ((unsigned char *)s2);
 	i = 0;
 	while (i < n)
 	{
-		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
-			return (s1[i] - s2[i]);
+		if (*text[0] != *text[1])
+			return (*text[0] - *text[1]);
 		i++;
 	}
 	return (0);
