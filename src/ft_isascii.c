5616416d7e27e4bfc7f554e852941af00847c182
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-20 17:59:44 -0300

    fix(ft_isascii): logic error correction.

--- src/ft_isascii.c
+++ src/ft_isascii.c
@@ -6,11 +6,11 @@
 /*   By: Isabela Genial <marvin@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 04:34:15 by Isabela Gen       #+#    #+#             */
-/*   Updated: 2023/05/19 04:34:19 by Isabela Gen      ###   ########.fr       */
+/*   Updated: 2023/05/20 16:20:08 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isdigit(int c)
+int	ft_isascii(int c)
 {
 	if (c >= 0 && c < 128)
 		return (1);
