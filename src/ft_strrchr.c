a230757ac3edf7ff5b2184b66266994cf7aa1139
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-20 18:02:03 -0300

    fix(ft_strrchr): logic error correction.

--- src/ft_strrchr.c
+++ src/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: igenial <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 16:22:18 by igenial           #+#    #+#             */
-/*   Updated: 2023/05/19 16:37:19 by igenial          ###   ########.fr       */
+/*   Updated: 2023/05/20 12:25:16 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ char	*ft_strrchr(const char *s, int c)
 	i = (ft_strlen(s));
 	while (i-- > 0)
 	{
-		if (((unsigned char *)s)[i] == ((unsigned char *)c))
-			return (s[i]);
+		if (((char *)s)[i] == c)
+			return ((char*)s);
 	}
 	return (0);
 }
