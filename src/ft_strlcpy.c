c427875c7395893afff208bd3700680d1f04c25b
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-23 19:23:39 -0300

    fix: logic error correction.

--- src/ft_strlcpy.c
+++ src/ft_strlcpy.c
@@ -6,17 +6,24 @@
 /*   By: igenial <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 12:01:32 by igenial           #+#    #+#             */
-/*   Updated: 2023/05/20 12:17:26 by igenial          ###   ########.fr       */
+/*   Updated: 2023/05/23 11:10:33 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
+{	
+	char	*s;
+
+	s = ((char *)src);
 	if (size == 0)
 		return (ft_strlen(src));
-	while (size-- > 0)
-		((unsigned char *)dst)[size] = ((unsigned char *)src)[size];
+	while ((size - 1) && *s)
+	{
+		*dst++ = *s++;
+		size--;
+	}
+	*dst = '\0';
 	return (ft_strlen(src));
 }
