4aaa85bb69e5fdd0bcbe0a5dad79f2d2a1c2200b
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-23 19:23:30 -0300

    fix: logic error correction.

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: igenial <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 12:29:23 by igenial           #+#    #+#             */
-/*   Updated: 2023/05/20 14:42:54 by igenial          ###   ########.fr       */
+/*   Updated: 2023/05/22 13:31:54 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	src_len = (ft_strlen(src));
 	i = 0;
 	if (dst_len >= size)
-		return (size + ft_strlen(src));
+		return (size + src_len);
 	while ((dst_len + i) < (size - 1) && src[i])
 	{
 		dst[dst_len + i] = ((char *)src)[i];
