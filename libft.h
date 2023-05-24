d4e37ee589bf41f1b33d74d072d7d6b8669621df
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-24 19:48:43 -0300

    fix(libft): this change add more functions in the script.

--- libft.h
+++ libft.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
 int	ft_atoi(const char *nptr);
@@ -27,5 +28,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void    *ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
