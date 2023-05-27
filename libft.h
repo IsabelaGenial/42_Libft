3f3d38f8b3c58050570f50d67a27f28abf7d4e6e
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-26 21:58:06 -0300

    fix(libft): this change add more functions in the script.

--- libft.h
+++ libft.h
@@ -31,5 +31,8 @@ char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+char	*ft_itoa(int n);
 
 #endif
