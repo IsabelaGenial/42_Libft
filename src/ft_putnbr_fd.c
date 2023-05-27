c5d77d7cd2fbda6538034c99d34f28a787a6affd
ISABELA GENIAL <isabelamgenial@gmail.com>
2023-05-26 22:06:32 -0300

    .

--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -6,32 +6,32 @@
 /*   By: Isabela Genial <marvin@42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 19:07:10 by Isabela Gen       #+#    #+#             */
-/*   Updated: 2023/05/24 19:36:09 by igenial          ###   ########.fr       */
+/*   Updated: 2023/05/26 16:58:17 by igenial          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	char position;
-	
+	char	position;
+
 	if (n == -2147483648)
 	{
 		write (fd, "-", 1);
 		write (fd, "2", 1);
-		n = 147483648;		
+		n = 147483648;
 	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		n *= -1;
 		write (fd, "-", 1);
 	}
-	else if (n / 10 == 0)
+	if (n / 10 == 0)
 	{
 		position = (n % 10) + '0';
 		write (fd, &position, 1);
-		return;
+		return ;
 	}
 	ft_putnbr_fd(n / 10, fd);
 	position = (n % 10) + '0';
